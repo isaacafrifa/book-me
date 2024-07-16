d662e03674037e87026cc140dfa74759e649c8d8
Afrifa <isaac.afrifa3@yahoo.com>
2024-07-17 01:15:48 +0300

    🧪feat(booking): create initial exceptions

---
+++ src/main/java/iam/bookme/exception/ExceptionController.java
@@ -0,0 +1,9 @@
+package iam.bookme.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ExceptionController extends ResponseEntityExceptionHandler {
+    
+}

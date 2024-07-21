d4813a6b959770875d31ed773876008d69992825
Afrifa <isaac.afrifa3@yahoo.com>
2024-07-21 17:13:09 +0300

    refactor(booking): use OffsetDateTime instead of LocalDateTime in Booking
    Decided to use OffsetDateTime so there's a match between api.yml and the entity model

--- src/main/java/iam/bookme/repository/BookingRepository.java
+++ src/main/java/iam/bookme/repository/BookingRepository.java
@@ -4,14 +4,14 @@ import iam.bookme.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
     // Find bookings starting after a specific time. It can be used to get upcoming bookings
-    List<Booking> findAllByStartTimeAfter(LocalDateTime specifiedTime);
+    List<Booking> findAllByStartTimeAfter(OffsetDateTime specifiedTime);
 
     List<Booking> findAllByUserEmail(String userEmail);
 
--- src/test/java/iam/bookme/TestContext.java
+++ src/test/java/iam/bookme/TestContext.java
@@ -1,28 +1,30 @@
 package iam.bookme;
 
+import iam.bookme.dto.BookingStatus;
 import iam.bookme.entity.Booking;
-import iam.bookme.enums.BookingStatus;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Getter
 public class TestContext {
 
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    // This pattern (XXX) includes the 3-digit zone offset (e.g. +05:30 for India Standard Time).
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
     public static final UUID bookingId = UUID.fromString("f81d4fae-7dec-11e4-9635-286e88f7c621");
 
     public Booking getTestBooking() {
         return new Booking(
                 null, // Let Hibernate generate ID
                 "test@email.com",
-                LocalDateTime.parse("2022-08-01T10:00:00Z", formatter),
-                LocalDateTime.parse("2022-08-01T10:00:00Z", formatter),
-                LocalDateTime.parse("2022-08-05T11:00:00Z", formatter),
+                OffsetDateTime.parse("2022-08-01T10:00:00+00:00", formatter),
+                OffsetDateTime.parse("2022-08-01T10:00:00+00:00", formatter),
+                OffsetDateTime.parse("2022-08-05T11:00:00+00:00", formatter),
                 45,
                 BookingStatus.PENDING,
                 "This is a test booking.");
     }
+
 }

caef79ab2935f76dd7c6e1f7ff361e7db58389ab
Afrifa <isaac.afrifa3@yahoo.com>
2024-07-27 21:25:26 +0300

    feat(booking): add openapi specification functionality
    This functionality adds auto documentation and assists creation of endpoints automatically

---
+++ src/main/java/iam/bookme/mapper/OrderByFieldMapper.java
@@ -0,0 +1,38 @@
+package iam.bookme.mapper;
+
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+This class is responsible for mapping the orderBy field from the API to the corresponding database field.
+It allows for adding new mappings and providing a default value for unknown fields.
+ */
+@NoArgsConstructor
+public class OrderByFieldMapper {
+    private final Map<String, String> mappings = new HashMap<>();
+    private String defaultValue;
+
+    public void addMapping(String apiField, String dbField) {
+        mappings.put(apiField, dbField);
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String map(String key) {
+        if (StringUtils.hasText(key)) {
+            Assert.isTrue(mappings.containsKey(key), "Field: [%s] is an unknown sorting field".formatted(key));
+        }
+        return this.mappings.getOrDefault(key, defaultValue);
+    }
+
+}

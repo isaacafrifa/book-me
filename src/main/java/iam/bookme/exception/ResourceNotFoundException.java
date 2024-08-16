e984331999ad9509e731b67bf130e91eefdca865
Isaac Afrifa <isaac.afrifa3@yahoo.com>
2024-08-16 23:24:32 +0300

    fix(booking): resolve http status in ResourceNotFoundException (#18)

--- src/main/java/iam/bookme/exception/ResourceNotFoundException.java
+++ src/main/java/iam/bookme/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package iam.bookme.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CREATED)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {

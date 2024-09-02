dedda73177a7eb71216d580beb59d9b68ff85a59
Afrifa <isaac.afrifa3@yahoo.com>
2024-09-02 23:49:17 +0300

    fix: resolve BookingMapper mappings

--- src/main/java/iam/bookme/dto/BookingMapper.java
+++ src/main/java/iam/bookme/dto/BookingMapper.java
@@ -2,6 +2,7 @@ package iam.bookme.dto;
 
 import iam.bookme.entity.Booking;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,16 @@ import org.springframework.stereotype.Component;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BookingMapper {
 
+    @Mapping(source = "startTime", target = "startTime")
+    @Mapping(source = "comments", target = "comments")
     Booking toEntity(BookingRequestDto bookingRequestDto);
 
+    @Mapping(source = "bookingId", target = "bookingId")
+    @Mapping(source = "startTime", target = "startTime")
+    @Mapping(source = "comments", target = "comments")
+    @Mapping(source = "userReferenceId", target = "userId")
+    @Mapping(source = "createdDate", target = "createdDate")
+    @Mapping(source = "updatedDate", target = "updatedDate")
+    @Mapping(source = "status", target = "bookingStatus")
     BookingDto toDto(Booking booking);
 }

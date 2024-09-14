a5b8fc4aef790ffdbc8f874b62a4cd39e5917e8d
Afrifa <isaac.afrifa3@yahoo.com>
2024-09-15 02:20:33 +0300

    refactor: update getUserFromUserServiceByEmail endpoint

--- src/main/java/iam/bookme/client/UserClient.java
+++ src/main/java/iam/bookme/client/UserClient.java
@@ -3,15 +3,15 @@ package iam.bookme.client;
 import iam.bookme.dto.UserDto;
 import iam.bookme.dto.UserRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "${user-feign.name}", url = "${user-feign.url}", configuration = FeignConfiguration.class)
 public interface UserClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/users/{userEmail}", consumes = "application/json")
-    UserDto getUserFromUserServiceByEmail(@PathVariable String userEmail);
+    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/users/email", consumes = "application/json")
+    UserDto getUserFromUserServiceByEmail(@RequestParam String userEmail);
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/v1/users")
     UserDto createUserInUserService(UserRequestDto userRequestDto);

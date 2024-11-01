f7774605a3350252deee4dd73a152cb7efa796ab
Isaac Afrifa <isaac.afrifa3@yahoo.com>
2024-11-01 02:46:31 +0200

    chore: update cucumber to use junit5 (#41)

--- src/test/java/cucumber/CucumberTestRunner.java
+++ src/test/java/cucumber/CucumberTestRunner.java
@@ -1,20 +1,30 @@
 package cucumber;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.test.context.ActiveProfiles;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 /**
  * This class is used to run the Cucumber tests.
  */
 
 @ActiveProfiles("cucumber")
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",
-        glue = "src/test/java/cucumber/steps",
-        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
-                "html:target/cucumber-reports/Cucumber.html"},
-        monochrome = true)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@SelectPackages({"cucumber.steps"})
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value =
+                """
+                        pretty,
+                        html:target/cucumber-reports/report.html,
+                        json:target/cucumber-reports/report.json""")
+
 public class CucumberTestRunner {
 }
